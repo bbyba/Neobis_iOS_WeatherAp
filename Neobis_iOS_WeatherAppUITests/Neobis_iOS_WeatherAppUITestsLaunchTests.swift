9323ed3d99d0a000458edbf5f4c67913c16c29fb
bbyba <burte.bsn@gmail.com>
2023-11-23 01:13:03 +0900

    Initial Commit

---
+++ Neobis_iOS_WeatherAppUITests/Neobis_iOS_WeatherAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Neobis_iOS_WeatherAppUITestsLaunchTests.swift
+//  Neobis_iOS_WeatherAppUITests
+//
+//  Created by Burte Bayaraa on 2023.11.23.
+//
+
+import XCTest
+
+final class Neobis_iOS_WeatherAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
