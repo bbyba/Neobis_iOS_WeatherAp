379699b973bdae4034d1ed38c37b3619f992a141
bbyba <burte.bsn@gmail.com>
2023-11-23 23:37:51 +0900

    Basic layout

---
+++ Neobis_iOS_WeatherApp/WeatherCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherCollectionViewCell.swift
+//  Neobis_iOS_WeatherApp
+//
+//  Created by Burte Bayaraa on 2023.11.23.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Neobis_iOS_WeatherApp/WeatherFirstView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherFirstView.swift
+//  Neobis_iOS_WeatherApp
+//
+//  Created by Burte Bayaraa on 2023.11.23.
+//
+
+import UIKit
+
+class WeatherFirstView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
